0afdd92481cb39793ed9553cca6d718b9eee7117
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2021-11-22 14:28:15 -0600

    ya quedo!
    ya jala despues de 3 intentos

--- AndroidStudioProjects/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ AndroidStudioProjects/TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -7,27 +7,48 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
     public static final String EXTRA_MESSAGE =
-            "com.example.android.twoactivities.extra.MESSAGE";//constant at the top of the class to define the key for the Intent extra
+            "com.example.android.activities.extra.MESSAGE";//constant at the top of the class to define the key for the Intent extra
     private EditText mMessageEditText; //Add a private variable at the top of the class to hold the EditText
+    public static final int TEXT_REQUEST = 1;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);//use findViewById() to get a reference to the EditText and assign it to that private variable:
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
 
     }
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
-        startActivity(intent);
         String message = mMessageEditText.getText().toString();//get the text from the EditText as a string
         intent.putExtra(EXTRA_MESSAGE, message);//Add that string to the Intent as an extra with the EXTRA_MESSAGE constant as the key and the string as the value:
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String reply =
+                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
