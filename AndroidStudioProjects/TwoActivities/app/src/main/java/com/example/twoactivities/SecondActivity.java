0c094d94d039921b6bfb57ac4c199a3ae16d892b
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2021-11-22 14:28:36 -0600

    zxczxc
    zxczxc

--- AndroidStudioProjects/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ AndroidStudioProjects/TwoActivities/app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -2,13 +2,35 @@ package com.example.twoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    public static final String EXTRA_REPLY =
+            "com.example.android.activities.extra.REPLY";
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mReply = findViewById(R.id.editText_second);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);//Get the string containing the message from the Intent extras using the MainActivity.EXTRA_MESSAGE static variable as the key:
+        TextView textView = findViewById(R.id.text_message);//Use findViewByID() to get a reference to the TextView for the message from the layout
+        textView.setText(message);//Set the text of the TextView to the string from the Intent extra
+
+    }
+
+    public void returnReply(View view) {
+        String reply = mReply.getText().toString();
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
     }
 }
