eadc9d3576425ac19ff5915e8aa5a44d58627017
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2021-11-22 12:47:58 -0600

    task2
    hasta aqui funciona todo bien

--- AndroidStudioProjects/Android_fundamentals_01_04/app/src/main/java/com/example/learntohelpyourselft/MainActivity.java
+++ AndroidStudioProjects/Android_fundamentals_01_04/app/src/main/java/com/example/learntohelpyourselft/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.learntohelpyourselft;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+
+
 }
---
+++ AndroidStudioProjects/Learn_to_help_yourselft/app/src/main/java/com/example/learn_to_help_yourselft/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.example.learn_to_help_yourselft.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
