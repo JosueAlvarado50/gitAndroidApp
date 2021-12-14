379d1c4af806be9b11c0811add31943af59e093c
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2021-12-13 19:28:48 -0600

    task 1 complete
    hasta aqui todo va bien

--- AndroidStudioProjects/Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ AndroidStudioProjects/Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,44 @@ public class MainActivity extends AppCompatActivity {
         //Find the TextViews by ID
         mScoreText1 = (TextView) findViewById(R.id.score_1);
         mScoreText2 = (TextView) findViewById(R.id.score_2);
-        
+
+    }
+
+    public void decreaseScore(View view) {
+    // Get de ID of the Button that was clicked
+        int viewID = view.getId();
+        switch (viewID){
+         // if it was on team 1
+            case R.id.decreaseTeam1:
+                //Decrement the score and update the TextView
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+          //if  it was Team 2
+            case R.id.decreaseTeam2:
+                // Drecrement the score and update de textview
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+
+    }
+
+    public void increaseScore(View view) {
+        // get de id of the button that was clicked
+        int viewID = view.getId();
+        switch (viewID){
+            //if it was on team 1
+            case R.id.increaseTeam1:
+                // Increment the score and update the textview
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+            // if it was team 2
+            case R.id.increaseTeam2:
+                //increment the score and update the TextView
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+
     }
 }
