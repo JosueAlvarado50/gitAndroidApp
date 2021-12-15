ce0afa15a5ec27e5a5a7502116deb27c760bae7f
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2021-12-15 05:39:12 -0600

    correciones
    correciones en el backgorund

--- AndroidStudioProjects/Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ AndroidStudioProjects/Scorekeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     private int mScore1;
     private int mScore2;
     private TextView mScoreText1;
@@ -19,52 +20,9 @@ public class MainActivity extends AppCompatActivity {
     static final String STATE_SCORE_2 = "Team 2 Score";
 
     @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        // Save the scores.
-        outState.putInt(STATE_SCORE_1, mScore1);
-        outState.putInt(STATE_SCORE_2, mScore2);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        //Check if the correct item was clicked
-        if(item.getItemId()==R.id.night_mode){
-            // Get the night mode state of the app.
-            int nightMode = AppCompatDelegate.getDefaultNightMode();
-            //Set the theme mode for the restarted activity
-            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
-                AppCompatDelegate.setDefaultNightMode
-                        (AppCompatDelegate.MODE_NIGHT_NO);
-            } else {
-                AppCompatDelegate.setDefaultNightMode
-                        (AppCompatDelegate.MODE_NIGHT_YES);
-            }
-// Recreate the activity for the theme change to take effect.
-            recreate();
-        }
-        return true;
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        // Change the label of the menu based on the state of the app.
-        int nightMode = AppCompatDelegate.getDefaultNightMode();
-        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
-            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
-        } else{
-            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
-        }
-        return true;
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Find the TextViews by ID
         mScoreText1 = (TextView) findViewById(R.id.score_1);
         mScoreText2 = (TextView) findViewById(R.id.score_2);
         if (savedInstanceState != null) {
@@ -75,45 +33,65 @@ public class MainActivity extends AppCompatActivity {
             mScoreText1.setText(String.valueOf(mScore1));
             mScoreText2.setText(String.valueOf(mScore2));
         }
-
-
     }
 
     public void decreaseScore(View view) {
-    // Get de ID of the Button that was clicked
-        int viewID = view.getId();
-        switch (viewID){
-         // if it was on team 1
+        int viewId = view.getId();
+        switch (viewId) {
             case R.id.decreaseTeam1:
-                //Decrement the score and update the TextView
                 mScore1--;
                 mScoreText1.setText(String.valueOf(mScore1));
                 break;
-          //if  it was Team 2
             case R.id.decreaseTeam2:
-                // Drecrement the score and update de textview
                 mScore2--;
                 mScoreText2.setText(String.valueOf(mScore2));
         }
-
     }
 
     public void increaseScore(View view) {
-        // get de id of the button that was clicked
-        int viewID = view.getId();
-        switch (viewID){
-            //if it was on team 1
+        int viewId = view.getId();
+        switch (viewId) {
             case R.id.increaseTeam1:
-                // Increment the score and update the textview
                 mScore1++;
                 mScoreText1.setText(String.valueOf(mScore1));
                 break;
-            // if it was team 2
             case R.id.increaseTeam2:
-                //increment the score and update the TextView
                 mScore2++;
                 mScoreText2.setText(String.valueOf(mScore2));
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        } else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.night_mode){
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            } else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            recreate();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
     }
 }
